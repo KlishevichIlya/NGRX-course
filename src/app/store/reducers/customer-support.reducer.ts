f36013cd2d9aabac56940a54f53d22a5fc0705e7
Ilya <ilya.klishevich@itechart-group.com>
2022-06-23 16:03:12 +0300

    Implemented first reducer

--- src/app/store/reducers/customer-support.reducer.ts
+++ src/app/store/reducers/customer-support.reducer.ts
@@ -1,4 +1,4 @@
-import { Action, createReducer, on } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { sendingCustomerSupportMessage } from '../actions/customer-support.actions';
 
 export const customerSupportFeatureKey = 'customerSupport';
